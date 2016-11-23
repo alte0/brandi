673b6c26481dd17f05f6619f39c38b7bdf32ac41
Максим <ctpagateji@yandex.ru>
2016-11-23 19:00:24 +0500

    проверка формы через jQuery c кастомным сообщением.

--- src/js/parts/code.js
+++ src/js/parts/code.js
@@ -1,21 +1,59 @@
 (function($, undefined) {
 	$(function() {
 		// code write
-		var $closeStaticMap = $('.discuss--map'); 
-		var $openJsMap = $('.discuss--map-js'); 
 
-		$closeStaticMap.addClass('discuss--map-jsInvisibleMap');
-		$openJsMap.addClass('discuss--map-jsShowMap');
+		var $closeStaticMap = $('.discuss--map');
+		var $openJsMap = $('.discuss--map-js');
+		var $searchiIframe = $openJsMap.find('iframe');
+
+		$searchiIframe.on('load', function() {
+				$closeStaticMap.addClass('discuss--map-jsInvisibleMap');
+				$openJsMap.addClass('discuss--map-jsShowMap');
+		});
+
+		//message error form
+		var modalWindow = $('<div>', {
+			class: 'modalWindow'
+		});
+		modalWindow.appendTo($('body'));
+
+		var messageBadForm = $('<div>', {
+			class: 'message',
+			text: 'Вы не заполнили поля формы, пожалуйста заполните все!'
+		});
+		messageBadForm.appendTo(modalWindow);
+
+		var buttonClose = $('<button>', {
+			class: 'buttonClose',
+			type: 'button',
+			text: 'Ok'
+		});
+		buttonClose.appendTo(messageBadForm);
 
 		$('form.discuss--message').submit(function() {
 			if (
-				$(this).find('input[name=name]').val() == '' ||
-				$(this).find('input[name=email]').val() == '' ||
-				$(this).find('textarea[name=message]').val() == '' ) {
-					alert('Вы не заполнили все поля формы, пожалуйста заполните все поля!');
-				return false;
-			}
-		});
+				$(this).find('input[name=name]').val() === '' ||
+				$(this).find('input[name=email]').val() === '' ||
+				$(this).find('textarea[name=message]').val() === '' ) {
+					// modalWindow.css('display', 'block');
+					modalWindow.fadeIn(300);
+					return false;
+				}
+			});
 
-	})
+			buttonClose.on('click', function() {
+				modalWindow.fadeOut(500);
+			});
+
+		//yandex map api
+		// var myMap;
+		// function init (ymaps) {
+		// 	myMap = new ymaps.Map("discuss--map-js", {
+		// 		center: [55.87, 37.66],
+		// 		zoom: 10
+		// 	});
+		//
+		// }
+
+	});
 })(jQuery);
